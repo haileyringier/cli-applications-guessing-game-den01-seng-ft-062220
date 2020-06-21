b5d9ccb5fb64cf1bdd42eb536cdcee2d4f1d9e7b
Hailey Ringier <haileyringier@outlook.com>
2020-06-21 21:28:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,22 @@
-# Code your solution here!
+
+def generate_number
+  rand 1..6
+end
+
+def instructions
+  puts "Guess a number between 1 and 6"
+end
+
+
+
+
+
+
+def run_guessing_game
+ generate_number
+ instructions
+ 
+ 
+  
+  
+end
