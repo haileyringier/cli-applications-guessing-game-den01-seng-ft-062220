f808291ae63107ea157651e70fe7cf91bd471636
Hailey Ringier <haileyringier@outlook.com>
2020-06-21 21:35:30 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -19,11 +19,16 @@ def run_guessing_game
  correct_answer = generate_number
  instructions
  player_guess = get_user_input
- if player_guess == correct_answer
+ if player_guess == "exit"
+  puts "Goodbye!"
+ elsif player_guess == correct_answer
   puts "You guessed the correct number"
  elsif player_guess != correct_answer
   puts"Sorry! The computer guessed #{correct_answer}."
- elsif player_guess == "exit"
-  puts "Goodbye!"
+
+
+
+#elsif player_guess == "exit"
+ # puts "Goodbye!"
 end
 end
